4afa790716ee2ca4a4e00ca6bc3bafeac6fc1425
DAVID MERCADO JUANES <dmercado@abc.gob.bo>
2025-02-14 17:40:33 -0400

    [FIRST COMMIT]

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => sprintf("pgsql:host=%s;dbname=%s", getenv('db_host'), getenv('db_name')),
+    'username' => sprintf("%s", getenv('db_user')),
+    'password' => sprintf("%s", getenv('db_pass')),
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ local/variable_local.sh
@@ -0,0 +1,7 @@
+export db_host=localhost
+export db_name=thebookclub
+export db_user=david
+export db_pass=s1st3m4s
+export db_pass=5432
+
+export salt=5HS46PWljkegnyTA5Tv4nf6WJNosGE!M
